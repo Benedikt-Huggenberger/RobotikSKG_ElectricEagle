f390e8cb84642846a98c8873961f27e8b193ab92
PlaneofMit <Benedikt.Huggenberger@gmail.com>
2026-07-10 16:37:30 +0200

    sa

---
+++ US_Sensoren_Berechnung.cpp
@@ -0,0 +1,18 @@
+//||
+#include <Wire.h>
+#include <utility/imumaths.h>
+#include "Ultrasonic.h"
+
+int AB1 = 0;
+int AB2 = 0;
+int AB3 = 0;
+int AB4 = 0;
+int AB_Sensoren = 100;      //mm
+int AB_Feld_x = 1100;
+int AB_Feld_y = 1800;
+
+Ultrasonic ultrasonic1(46);
+Ultrasonic ultrasonic2(48);
+Ultrasonic ultrasonic3(50);
+Ultrasonic ultrasonic4(52);
+
