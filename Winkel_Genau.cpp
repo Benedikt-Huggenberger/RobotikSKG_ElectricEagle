7bcfe97304e99b74c678de1a9448f9e5e3282506
PlaneofMit <Benedikt.Huggenberger@gmail.com>
2026-07-13 22:18:21 +0200

    kp alter

--- Winkel_Genau.cpp
+++ Winkel_Genau.cpp
@@ -14,28 +14,34 @@ int AB_Feld_x = 0;
 int AB_Feld_y = 0;
 int niedrigwert;
 int werte[15];
+int naechster = 0;
+int vorheriger = 0;
 
 int main() {
     return 0;
 }
 
-int Winkel_Genau(){                                      //checken welcher sensor von den beiden benachtbarten näher ist
-    if(werte[sensor + 1] < werte[sensor - 1]) {          //gegen uhrzeigersinn größer
-        Diff_sensoren(true);
-    }
-    else if(werte[sensor + 1] > werte[sensor - 1]) {     //im uhrzeigersinn größer
 
-    }
-    else {
-        return(sensor*22,5);
-    }
-}
-
-int Diff_sensoren(bool größer) {
+double Diff_sensoren(bool größer) {
     if (größer == true) {
-        return((werte[sensor + 1]/werte[sensor - 1])*100);
+        return (double)werte[sensor + 1] / werte[sensor - 1];
     }
     else if (größer == false) {
-        return((werte[sensor - 1]/werte[sensor])*100);
+        return (double)werte[sensor - 1] / werte[sensor - 1];
     }
 }
+
+double Winkel_Genau(){                                    //checken welcher sensor von den beiden benachtbarten näher ist
+    int naechster = (sensor + 1) % 15;
+    int vorheriger = (sensor - 1 + 15) % 15;
+    if(werte[naechster] < werte[vorheriger]) {          //gegen uhrzeigersinn größer
+        return((sensor * 22.5)+(Diff_sensoren(true)*22.5));
+    }
+    else if(werte[naechster] > werte[vorheriger]) {     //im uhrzeigersinn größer
+        return((sensor * 22.5)-(Diff_sensoren(true)*22.5));
+    }
+    else {
+        return(sensor*22.5);
+    }
+}
+
